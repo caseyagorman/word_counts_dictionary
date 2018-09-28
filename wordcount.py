a6402ac077f03fd2a9bf5cd1f5a5aa017087bcba
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-27 20:58:42 -0700

    add sorted dictionary function

--- wordcount.py
+++ wordcount.py
@@ -1,23 +1,24 @@
 # count words in poem
 import re
-from collections import Counter
+from collections import Counter, OrderedDict
 
-def word_count(filename):
-	word_dict = {}
 
-	text = open(filename, "r")
-	for line in text:
-		line = line.rstrip()
-		line = re.sub(r'[^\w\s]','',line)
-		line = line.lower()
-		line = line.split(" ")
-		for word in line:
-			word_dict[word] = word_dict.get(word, 0) + 1
-	for key, value in word_dict.items():
-		print (key, value)	
-	return word_dict
+# def word_count(filename):
+# 	word_dict = {}
 
-word_count("test.txt")
+# 	text = open(filename, "r")
+# 	for line in text:
+# 		line = line.rstrip()
+# 		line = re.sub(r'[^\w\s]','',line)
+# 		line = line.lower()
+# 		line = line.split(" ")
+# 		for word in line:
+# 			word_dict[word] = word_dict.get(word, 0) + 1
+# 	for key, value in word_dict.items():
+# 		print (key, value)	
+# 	return word_dict
+
+# word_count("test.txt")
 
 
 # Further Study
@@ -33,9 +34,21 @@ def counter_count(filename):
 		for word in line:
 			big_list.append(word)
 	word_dict = Counter(big_list)
-	print (word_dict)
+	return word_dict
 
-counter_count("test.txt")
+def sorted_dict(word_dict):
+	sorted_dict = OrderedDict(sorted(word_dict.items()))
+	print(sorted_dict)	
+	# sorted_dict = sorted(word_dict.keys())
+	# for item in sorted_dict:
+	# 	print (item, word_dict[item])
+
+word_dict = counter_count("test.txt")
+sorted_dict(word_dict)
+
+
+
+
 
 	
 
